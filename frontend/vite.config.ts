211af64182c083fcb590ebedfa87dd46f5748bf4
Michal Smyrnow <dsw49561@student.dsw.edu.pl>
2025-05-30 18:24:37 +0200

    dodanie testów SearchBar i SearchHistory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
@@ -26,5 +27,11 @@ export default defineConfig({
   },
   define: {
     'process.env': {}
+  },
+  test: {
+    globals: true,                     
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    exclude: [...configDefaults.exclude, 'e2e']
   }
 });
